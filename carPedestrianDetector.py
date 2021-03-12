d5c7f566838d0fe33614a3bbfb5701be101ec519
John Solomon <jolomonson@gmail.com>
2021-03-12 19:56:51 +0100

    Detecting an Image

--- carPedestrianDetector.py
+++ carPedestrianDetector.py
@@ -1,17 +1,28 @@
 import cv2
 from random import randrange
 #our image file
-img = cv2.imread('Images/cars3.png')
+img = cv2.imread('Images/carFleet.jfif')
 #our pre-trained car classifier
 carTracker = cv2.CascadeClassifier('carDetector.xml')
 #Convert to grayscaled image
-grayScaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Classfying Image
-trainedImage = carTracker.detectMultiScale(grayScaled)
+trainedImage = carTracker.detectMultiScale(grayImg)
 #print(trainedImage)
+print(trainedImage)
 #Draw Rectangles
-for x,y,w,h in trainedImage:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)))
-cv2.imshow('Fleet of cars', img)
+jed = [cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 1) for (x,y,w,h) in trainedImage]
+#Displaying Images
+cv2.imshow('Car Detector', img)
 cv2.waitKey()
 print('Code Completed')
+
+'''
+# Video Detection
+video = cv2.VideoCapture('Videos/')
+trainedClassifier = cv2.CascadeClassifier('carDetector.xml')
+while True:
+    #Reading Frame
+    read_successful, frame = video.read()
+    
+'''
