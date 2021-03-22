8f615f965adbe4e906759724a77a353c5f88cc55
John Solomon <jolomonson@gmail.com>
2021-03-22 04:04:00 +0100

    Video Detection with cars

--- carPedestrianDetector.py
+++ carPedestrianDetector.py
@@ -23,3 +23,27 @@ cv2.imshow('Car Detector[Image]', img)
 cv2.waitKey()
 print('Code Completed')
 '''
+# Video Detection with Cars
+import cv2
+from random import randrange
+video = cv2.VideoCapture('Videos/Pedestrians Compilation_1080pFHR.mp4')
+trainedClassifier = cv2.CascadeClassifier('carDetector.xml')
+while True:
+    #Reading Frame
+    read_successful, frame = video.read()
+    if read_successful:
+        #Covert to grayScale
+        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else:
+        break
+    #Detecting Cars
+    trainedFrames = trainedClassifier.detectMultiScale(grayFrame)
+    #Drawing Rectangles
+    coordinates = [cv2.rectangle(frame, (x,y), (x+w, y+h), (randrange(256),randrange(256),randrange(256))) for (x,y,w,h) in trainedFrames]
+    cv2.imshow('Car Detector[Video]', frame)
+    key = cv2.waitKey(1)
+    # Stop if Q is pressed
+    if key == 81 or key == 113:
+        break
+video.release()
+#Pedestrians Detection
